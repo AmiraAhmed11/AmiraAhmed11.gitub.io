315b1fe90b425a685d835747af88951aceeae284
sara elbaroudy <139095836+saraelbarourdy@users.noreply.github.com>
2024-05-08 22:56:53 +0300

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'moslem';
-$conn = mysqli_connect($servername, $username, $password, $db_name) or die ('connection failled');
-
-?>
